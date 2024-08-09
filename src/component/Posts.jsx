06d7c13b10d0d86e641a8e9ff0f22acb1dbd89bc
Sanjay Duwal <sanzay8342258@gmail.com>
2024-08-08 21:46:53 -0500

    added readme information on the project

--- src/component/Posts.jsx
+++ src/component/Posts.jsx
@@ -2,16 +2,48 @@ import React from "react";
 
 const Posts = () => {
   const items = [
-    { id: "", message: "It was so boring day." },
-    { id: "", message: "It was so boring day." },
-    { id: "", message: "It was so boring day." },
-    { id: "", message: "It was so boring day." },
-    { id: "", message: "It was so boring day." },
+    {
+      id: "",
+      title: "Today activity",
+      message: "It was so boring day.",
+      date: "August 08 at 08:08pm",
+    },
+    {
+      id: "",
+      title: "Today activity",
+      message: "It was so boring day.",
+      date: "August 08 at 08:08pm",
+    },
+    {
+      id: "",
+      title: "Today activity",
+      message: "It was so boring day.",
+      date: "August 08 at 08:08pm",
+    },
+    {
+      id: "",
+      title: "Today activity",
+      message: "It was so boring day.",
+      date: "August 08 at 08:08pm",
+    },
+    {
+      id: "",
+      title: "Today activity",
+      message: "It was so boring day.",
+      date: "August 08 at 08:08pm",
+    },
+    {
+      id: "",
+      title: "Today activity",
+      message: "It was so boring day.",
+      date: "August 08 at 08:08pm",
+    },
   ];
   return (
     <div className="container posts">
       {items.map((item, index) => (
         <div className="post" key={index}>
+          {item.title}
           {item.message}
         </div>
       ))}
